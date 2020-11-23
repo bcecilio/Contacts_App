3b54a7d19b7bb5674de8a7a67cd29f96190a92ad
brendon cecilio <brendon.cecilio@gmail.com>
2020-11-22 19:22:09 -0500

    starting proj files

---
+++ Contacts_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Contacts_app
+//
+//  Created by Brendon Cecilio on 11/22/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
