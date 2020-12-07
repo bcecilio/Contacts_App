bead332c06e6ce113b6be356128a998c38b8ff53
brendon cecilio <brendon.cecilio@gmail.com>
2020-12-07 16:04:39 -0500

    set up table view func in main vc

--- Contacts_app/View Controllers/ViewController.swift
+++ Contacts_app/View Controllers/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let contactView = ContactListView()
+    
+    var contacts = [Contacts]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view = contactView
+        setupTableView()
+    }
+    
+    func setupTableView() {
+        contactView.tableView.delegate = self
+        contactView.tableView.dataSource = self
+        contactView.tableView.register(ContactCell.self, forCellReuseIdentifier: "contactCell")
     }
 
+}
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contacts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
 }
 
