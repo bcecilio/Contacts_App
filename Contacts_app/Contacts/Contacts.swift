fd98387ba56db11a10fdaf12db1fd9f4d31722dc
brendon cecilio <brendon.cecilio@gmail.com>
2020-12-09 17:59:42 -0500

    added dummy data for contacts

--- Contacts_app/Contacts/Contacts.swift
+++ Contacts_app/Contacts/Contacts.swift
@@ -10,5 +10,15 @@ import UIKit
 struct Contacts {
     var contactName: String
     var contactNumber: Int
-    var contactImage: UIImage
+//    var contactImage: UIImage
+    
+    static func getContacts() -> [Contacts] {
+        let contactNames = ["James Hoffa", "Frank Sheeran", "Russel Buffalino", "Tony Salerno"]
+        var contacts = [Contacts]()
+        for name in contactNames {
+            let contact = Contacts(contactName: name, contactNumber: 8001234567)
+            contacts.append(contact)
+        }
+        return contacts
+    }
 }
